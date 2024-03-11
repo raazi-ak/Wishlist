26a5f3bbcd129a75f06be8b82b572cb52ab2ffb1
Raazi Faisal <raazi.faisal@gmail.com>
2024-03-11 23:56:04 +0530

    commit 8

--- app/src/main/java/com/raazi/wishlist/WishViewModel.kt
+++ app/src/main/java/com/raazi/wishlist/WishViewModel.kt
@@ -4,8 +4,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.raazi.wishlist.data.Wish
+import com.raazi.wishlist.data.WishRepository
+import kotlinx.coroutines.flow.Flow
 
-class WishViewModel:ViewModel(){
+class WishViewModel(
+    private val wishRepository: WishRepository
+):ViewModel(){
+
     var wishTitleState by mutableStateOf("")
     var wishDescState by mutableStateOf("")
 
@@ -18,5 +24,5 @@ class WishViewModel:ViewModel(){
         wishDescState = newString
     }
 
-
+    lateinit var getAllWishes:Flow<List<Wish>>
 }
